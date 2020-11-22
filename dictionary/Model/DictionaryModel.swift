34d5d3b22cd10a966bf29479853c0858919c38ab
Sunny Duong <sunnyduong8@gmail.com>
2020-11-21 22:51:59 -0800

    Dictionary App

---
+++ dictionary/Model/DictionaryModel.swift
@@ -0,0 +1,18 @@
+//
+//  DictionaryModel.swift
+//  MinimalDictionary
+//
+//  Created by Sunny Duong on 11/20/20.
+//
+
+import Foundation
+struct DictionaryModel{
+    let define: [String]
+    let name: String
+    let partOfSpeech : [String]
+    //let examples: String
+    //let synonyms:[Int:String]
+    let pronunciation:String
+    //let etymology: String
+    //let audioURL:String
+}
